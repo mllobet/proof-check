000a281b8ddcb99c92c52880f8b808c65921bf16
Kevin Devroede <kevin.devroede@epitech.eu>
2013-07-22 16:12:15 -0700

    Add the parser and the token class

---
+++ src/Token.java
@@ -0,0 +1,33 @@
+
+public class Token
+{
+	public enum Type
+	{
+		VARIABLE,
+		OPEN_PARENTHESIS,
+		CLOSE_PARENTHESIS,
+		UNARY_NOT_OPERATOR,
+		BIN_AND_OPERATOR,
+		BIN_OR_OPERATOR,
+		EQUALS_OPERATOR,
+	}
+	
+	private Type _type;
+	private String _value;
+	
+	public Token(Type type, String value)
+	{
+		_type = type;
+		_value = value;
+	}
+	
+	public Type getType()
+	{
+		return _type;
+	}
+	
+	public String getValue()
+	{
+		return _value;
+	}
+}
