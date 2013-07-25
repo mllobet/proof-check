9e804c4f003b99bf2b531c9fdd009abde91208d7
Kevin Devroede <kevin.devroede@epitech.eu>
2013-07-25 16:53:48 -0700

    git, wtf ?

---
+++ src/ShowCommand.java
@@ -0,0 +1,32 @@
+
+public class ShowCommand extends Command
+{
+	public ShowCommand(LineNumber lineNumber, Expression expr, Command parent)
+	{
+		super(lineNumber, expr, parent);
+	}
+	
+	public ShowCommand(LineNumber lineNumber, Expression expr, Command parent, String... args)
+	{
+		super(lineNumber, expr, parent, args);
+	}
+
+	@Override
+	public void execute()
+	{
+		
+	}
+	
+	@Override
+	public boolean isComplete()
+	{
+		return false;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "show";
+	}
+
+}
---
+++ src/TheoremSet.java
@@ -0,0 +1,11 @@
+public class TheoremSet {
+
+	public TheoremSet ( ) {
+		
+	}
+
+	public Expression put (String s, Expression e) {
+		
+		return null;
+	}
+}
