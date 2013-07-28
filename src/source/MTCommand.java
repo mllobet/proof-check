da3f51de5375d96f6adc2b5f1ddf18060cfd0c26
Miquel Llobet <mllobet.cm@gmail.com>
2013-07-27 21:58:18 -0700

    mp and mt work

---
+++ src/source/MTCommand.java
@@ -0,0 +1,62 @@
+package source;
+
+import java.util.List;
+
+public class MTCommand extends Command
+{
+
+	public MTCommand(LineNumber lineNumber, Expression expr, Command parent)
+	{
+		super(lineNumber, expr, parent);
+	}
+
+	public MTCommand(LineNumber lineNumber, Expression expr, Command parent, String... args)
+	{
+		super(lineNumber, expr, parent, args);
+	}
+
+	@Override
+	public void execute(List<Command> commands) throws IllegalLineException
+	{
+		// ~E2 and (E1=>E2) we can infer ~E1
+		if (commands.get(0).getInference().getTree().equalsOpositeSign((ProofNode)commands.get(1).getInference().getTree().root.getRight()) && getExpr().getTree().equalsOpositeSign((ProofNode)commands.get(1).getInference().getTree().root.getLeft())) {
+			if (debug) {
+				System.out.println(commands.get(0).getInference().getTree().equalsOpositeSign((ProofNode)commands.get(1).getInference().getTree().root.getRight()));
+				System.out.println(getExpr().getTree().equalsOpositeSign((ProofNode)commands.get(1).getInference().getTree().root.getLeft()));
+			}
+			
+			setInference(getExpr());
+		}
+		else if (commands.get(1).getInference().getTree().equalsOpositeSign((ProofNode)commands.get(0).getInference().getTree().root.getRight()) && getExpr().getTree().equalsOpositeSign((ProofNode)commands.get(0).getInference().getTree().root.getLeft())) {
+			if (debug) {
+				System.out.println(commands.get(1).getInference().getTree().equalsOpositeSign((ProofNode)commands.get(0).getInference().getTree().root.getRight()));
+				System.out.println(getExpr().getTree().equalsOpositeSign((ProofNode)commands.get(0).getInference().getTree().root.getLeft()));
+			}
+			
+			setInference(getExpr());
+		}
+		else 
+			throw new IllegalLineException("MT has invalid input");
+
+	}
+
+	@Override
+	public boolean isOk()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean isComplete()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "mc";
+	}
+
+}
