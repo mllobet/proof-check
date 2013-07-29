aa101f6fbcaa903d02998614117591f4443b5e62
Miquel Llobet <mllobet.cm@gmail.com>
2013-07-28 20:37:25 -0700

    added a dummy theorems file

---
+++ src/testing/ExpressionTest.java
@@ -0,0 +1,29 @@
+package testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExpressionTest {
+
+	@Test
+	public void testExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsImplication() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTree() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/testing/ProofTest.java
@@ -0,0 +1,27 @@
+package testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProofTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testFinishProof() {
+		
+	}
+	
+	public void finishSubProof() {
+		
+	}
+	
+	public void finishProofWithSubProof() {
+		
+	}
+
+}
